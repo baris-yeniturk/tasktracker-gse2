4b46e293e7f1bcf7254b4ce4c1e11556dfa257b0
Muzaffer Yenituerk <myenituerk@stud.hs-heilbronn.de>
2025-12-01 23:10:13 +0100

    GRÜN: Validierung für leere Beschreibung

--- src/main/java/com/tasktracker/model/Task.java
+++ src/main/java/com/tasktracker/model/Task.java
@@ -4,6 +4,9 @@ public class Task{
     private String description;
 
     public Task(String description){
+        if (description == null || description.trim().isEmpty()){
+            throw new IllegalArgumentException("No description found");
+        }
         this.description = description;
     }
 
