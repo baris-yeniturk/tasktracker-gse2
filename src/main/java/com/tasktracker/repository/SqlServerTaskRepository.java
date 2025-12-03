b9e01c0e124afa9088a47d8ba6e3bdb22e2c2530
Muzaffer Yenituerk <myenituerk@stud.hs-heilbronn.de>
2025-12-03 19:23:22 +0100

    Task Repo für SQL Server einrichten

--- src/main/java/com/tasktracker/repository/SqlServerTaskRepository.java
+++ src/main/java/com/tasktracker/repository/SqlServerTaskRepository.java
@@ -1,4 +1,25 @@
 package com.tasktracker.repository;
 
-public class SqlServerTaskRepository {
+import com.tasktracker.model.Task;
+import com.tasktracker.model.TaskStatus;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
+
+public class SqlServerTaskRepository{
+    private final String connectionUrl;
+    private final String username;
+    private final String password;
+
+    public SqlServerTaskRepository(String connectionUrl, String username, String password){
+        this.connectionUrl = connectionUrl;
+        this.username = username;
+        this.password = password;
+        validateConnection();
+    }
+
+
 }
