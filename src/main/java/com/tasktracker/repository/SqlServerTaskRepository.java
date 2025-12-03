98360f2b5efa244128d864fe344b6dbb7eb14634
Muzaffer Yenituerk <myenituerk@stud.hs-heilbronn.de>
2025-12-03 19:35:40 +0100

    FEAT: Weitere Methode

--- src/main/java/com/tasktracker/repository/SqlServerTaskRepository.java
+++ src/main/java/com/tasktracker/repository/SqlServerTaskRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
 
-public class SqlServerTaskRepository{
+public class SqlServerTaskRepository implements TaskRepository{
     private final String connectionUrl;
     private final String username;
     private final String password;
@@ -19,6 +19,59 @@ public class SqlServerTaskRepository{
         this.username = username;
         this.password = password;
         validateConnection();
+    }
+
+    public SqlServerTaskRepository(Properties properties){
+        this(
+                properties.getProperty("mssql.url", "jdbc:sqlserver://localhost:1433;"+
+                        "databaseName=TaskTrackedDB;trustServerCertificate=true"),
+                properties.getProperty("mssql.username", "sa"),
+                properties.getProperty("mssql.password", ""));
+    }
+
+    public SqlServerTaskRepository(){
+        this(
+                "jdbc:sqlserver://localhost:1433;databaseName=TaskTrackerDB;trustServiceCertificate=true",
+                "sa", ""
+        );
+    }
+
+    private void validateConnection(){
+
+    }
+
+    private Connection getConnection(){
+        return null;
+    }
+
+    @Override
+    public Task save(Task task){
+        return null;
+    }
+
+    @Override
+    public Optional<Task> findById(long id){
+        return null;
+    }
+
+    @Override
+    public List<Task> findAll(){
+        return null;
+    }
+
+    @Override
+    public Task update(Task task){
+        return null;
+    }
+
+    @Override
+    public boolean delete(long id){
+        return false;
+    }
+
+    @Override
+    public boolean existsById(long id){
+        return false;
     }
 
 
